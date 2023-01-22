d68bcff5b25e64835e34b4e9de3b518b6c26bbcc
Sergey Sych <vglazasmotri@yandex.ru>
2023-01-23 01:31:50 +0300

    Использовал dataclasses. Убрал 'одноразовые' переменные. Добавил обработку ошибок с несуществующими типами тренировок и/или невалидными данными. Push try 2.

--- homework.py
+++ homework.py
@@ -4,26 +4,19 @@ from dataclasses import asdict, dataclass
 @dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def __init__(self,
-                 training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float
-                 ) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
+    TEXT: str = ('Тип тренировки: {training_type}; '
+                 'Длительность: {duration:.3f} ч.; '
+                 'Дистанция: {distance:.3f} км; '
+                 'Ср. скорость: {speed:.3f} км/ч; '
+                 'Потрачено ккал: {calories:.3f}.')
 
-    def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.'
-                )
+    def get_message(self):
+        return self.TEXT.format(**asdict(self))
 
 
 class Training:
